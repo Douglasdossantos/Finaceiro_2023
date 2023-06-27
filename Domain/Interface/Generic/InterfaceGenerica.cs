9f46e8b08e56bb064f327a321a9f1178ea00499f
Douglas costa <douglasalgustocosta@gmail.com>
2023-06-26 22:36:11 -0300

    Criação de alguma interfaces

---
+++ Domain/Interface/Generic/InterfaceGenerica.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interface.Generic
+{
+    public interface InterfaceGenerica<T> where T : class
+    {
+        Task Add(T Objeto);
+        Task Update(T Objeto);
+        Task Delete(T Objeto);
+        Task<T> getEntityById(int Id);
+        Task<List<T>> list();
+    }
+}
